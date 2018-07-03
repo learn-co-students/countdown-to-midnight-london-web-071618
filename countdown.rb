5253417a2ea5b1cfd074a42b787f21a6db0275e2
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 17:22:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,15 @@ def countdown(number)
     puts "#{n} SECOND(S)!"
   n -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
-
+  
+def countdown_with_sleep(number)
+  n = number
+  while n > 0
+    puts "#{n} SECOND(S)!"
+  n -= 1
+  sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
